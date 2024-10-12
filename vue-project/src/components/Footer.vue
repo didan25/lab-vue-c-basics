e50dc3760cd46c54c664bfba183e323b5edc4d0a
Ana Jerkovic <ajerkovic31@yahoo.com>
2024-10-12 21:27:02 +0200

    done

---
+++ vue-project/src/components/Footer.vue
@@ -0,0 +1,32 @@
+
+<script setup>
+import { ref } from 'vue';
+
+const isDiscountAvailable = ref(false);
+
+</script>
+
+<template>
+    <div>
+        <button @click="isDiscountAvailable = !isDiscountAvailable">Check if you are eligible for a disocunt</button>
+
+        <!-- V-IF directive  -->
+        <p v-if="isDiscountAvailable">Enjoy 20% discount on all orders!</p>
+        <!-- V-SHOW directive  -->
+        <p v-show="isDiscountAvailable">Enjoy 50% discount for your next order!</p>
+    </div>
+  
+</template>
+
+<style scoped>
+
+button {
+    
+    background-color: rgb(254, 239, 68);
+    width: 150px;
+    height: 40;
+    padding: 20px;
+    border-radius: 10px;
+
+}
+</style>
---
+++ vue-project/src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const posts= ref([
+    {
+        title: "How to learn VUE",
+        description: "best practices for learning",
+        content: "directives, components and two way data binding"
+    },
+    {
+        title: "JS Conditionals and Loops",
+        description:"Use the if..else statement",
+        content: "Identify when it’s better to use switch over if..else"
+    },
+    {
+        title: "Web page structure",
+        description: "HTML and CSS",
+        content: "Understand margin, padding and border properties, and the differences between them",
+    }
+]);
+
+</script>
+
+<template>
+    <div>
+    <div v-for="(post, index) in posts" :key="index">
+        <h2> {{ post.title }}</h2>
+        <p>Description:</p>
+        <p>Content:</p>
+    </div>
+</div>
+  
+</template>
+
+<style scoped >
+
+</style>
+
